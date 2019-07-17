092d35fd505df2cb9d58fc304c12963d5a1c60b2
Holybasil <holybasil.1128@gmail.com>
2019-07-17 16:03:17 +0800

    fix export

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: ["@vue/app"]
+  // presets: [["@vue/app", { modules: "commonjs" }]],
+  presets: [["@vue/app", { modules: "cjs" }]],
+  plugins: ["add-module-exports"]
 };
--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import * as components from "./components";
 const install = function(Vue, options = {}) {
   for (const componentName in components) {
     const component = components[componentName];
-
     Vue.component(component.name, component);
   }
 };
-
-export default { install, ...components };
+// const exportComponents  = {}
+// for (const componentName in components) {
+//   exportComponents[componentName] = components[componentName];
+// }
+console.log(components, 'components')
+export { ...components };
+export default { install };
 
 if (typeof window !== "undefined" && window.Vue) {
   install(window.vue);
