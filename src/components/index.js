fb1e066bedab5706baffce0f0647775a40b42dfd
Holybasil <holybasil.1128@gmail.com>
2019-07-19 10:22:52 +0800

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
 export { default as HolyClickoutside } from "./holyClickoutside/index.js";
 export { default as HolyList } from "./holyList/index.js";
-export { default as HolyBase } from "./holyBase";
